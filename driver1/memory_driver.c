a464fb0203f41f4093a9de440ac8ae8cdef5454a
Palash Jain <palash.jain@gilbarco.com>
2017-12-08 11:50:50 +0530

    memory allocated and freed in init and exit repectively

--- driver1/memory_driver.c
+++ driver1/memory_driver.c
@@ -1,13 +1,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/slab.h>
 
+char *k = NULL;
 static int hello_init(void){
-    printk(KERN_ALERT "Hello World!!\n");
+    printk(KERN_ALERT "Allocating 4K memory to kernel buffer\n");
+    k= (char *)kmalloc(4096,GFP_KERNEL);
+    memcpy(k,"This is in memory\0",50);
     return 0;
 }
 
 static int hello_exit(void){
-    printk(KERN_ALERT "Good modi World!!\n");
+    printk(KERN_ALERT "Checkign wether memory persist %s!\n",k);
+    kfree(k);
     return 0;
 }
 
