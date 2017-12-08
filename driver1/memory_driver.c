546bd4e91df28fd85f8d9df53634468362ddc543
Palash Jain <palash.jain@gilbarco.com>
2017-12-08 16:32:22 +0530

    device will reflect in /proc/devices Memory driver

--- driver1/memory_driver.c
+++ driver1/memory_driver.c
@@ -1,18 +1,36 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/types.h>
+#include <linux/fs.h>
+#define MAJOR_NUM 2
+#define MINOR_NUM 4
+#define DRIVER_NAME "Memory_Driver"
+#define NO_OF_DEV 1
+
+dev_t deviceNum;
 
 char *k = NULL;
 static int hello_init(void){
-    printk(KERN_ALERT "Allocating 4K memory to kernel buffer\n");
+    int ret=0;
+    printk(KERN_ALERT " %s: Allocating 4K memory to kernel buffer\n" ,DRIVER_NAME);
     k= (char *)kmalloc(4096,GFP_KERNEL);
     memcpy(k,"This is in memory\0",50);
+    deviceNum= MKDEV(MAJOR_NUM,MINOR_NUM);
+    ret = register_chrdev_region(deviceNum,NO_OF_DEV,DRIVER_NAME);
+    if(ret < 0){
+        printk(KERN_ERR "Not able to allocate device number %d \n",ret);
+        kfree(k);
+        return ret;
+    }
+    printk(KERN_ALERT "%s: Major Number %d Minor Number %d\n", DRIVER_NAME, MAJOR(deviceNum),MINOR(deviceNum));
     return 0;
 }
 
 static int hello_exit(void){
-    printk(KERN_ALERT "Checkign wether memory persist %s!\n",k);
+    printk(KERN_ALERT "%s: Checking wether memory persist %s!\n",DRIVER_NAME,k);
     kfree(k);
+    unregister_chrdev_region(deviceNum,NO_OF_DEV);
     return 0;
 }
 
